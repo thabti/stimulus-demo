64e041fb330c5815bed2b49cc07d5bc420e0a410
Sabeur Thabti <thabti.sabeur@gmail.com>
2017-12-31 18:52:02 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const { Application, Controller } = require('stimulus'); 
+// class HelloController extends Controller {
+
+// }
+
+// const application = Application.start();
+// application.register('hello', HelloController);
